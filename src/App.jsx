5e3875d0649f0ce4c8ccbf697e759eb655a64bf3
emanuel-LinuxPC <ema4hhh@gmail.com>
2025-01-02 15:49:48 -0500

    track file component

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import { useState } from 'react'
 import ImageCanvas from './components/ImageCanvas'
 import ImageInput from './components/ImageInput'
 
+function AppInfo({ info, fileInfo }) {
+  return (
+    <div id="app-info">
+      <p>{info || "Nothing to say"}</p>
+      <p>{fileInfo || "No file"}</p>
+    </div>
+  )
+}
+
 function App() {
   const [texture, setTexture] = useState('zenitsu.png')
+  const [info, setInfo] = useState("")
+  const [fileInfo, setFileInfo] = useState("")
   
   return (
     <>
-      <ImageInput setTexture={setTexture} />
+      <ImageInput setTexture={setTexture} setInfo={setInfo} setFileInfo={setFileInfo} />
       <ImageCanvas texture={texture} />
+      <AppInfo info={info} fileInfo={fileInfo} />
     </>
   )
 }
